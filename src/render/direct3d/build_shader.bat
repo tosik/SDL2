c19752507d6bda4255be26fcb87b6e0a0f9654b0
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2019-02-04 10:33:23 -0800

    Add direct3d shader compilation command and an example HLSL code

---
+++ src/render/direct3d/build_shader.bat
@@ -0,0 +1,1 @@
+"C:/Program Files (x86)/Microsoft DirectX SDK (June 2010)/Utilities/bin/x86/"fxc.exe src/render/direct3d/shader1.hlsl /T ps_2_0 /Fo"src/render/direct3d/shader1.output"
---
+++ src/render/direct3d/build_shader.sh
@@ -0,0 +1,2 @@
+./src/render/direct3d/build_shader.bat
+hexdump -v -e '6/4 "0x%08.8x, " "\n"' src/render/direct3d/shader1.output
